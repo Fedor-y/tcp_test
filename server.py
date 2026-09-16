6b06a565c7cb9d5b2d15c5155473cf50138802d8
Fedor <fedorpuckov9@gmail.com>
2026-09-16 22:38:42 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,19 @@
+import socket
 
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.bind(('0.0.0.0',1234)) #привязываем к порту 1234
+server.listen()
+print('сервер запущен \n\n')
+while True:
+ #принимаем данные
+    client_sock , client_inf = server.accept()
+    data = (client_sock.recv(1024)).decode()
+
+    #сохраняем данные отправителя чтобы потом отправить ответ
+    client_ip, client_port =client_inf[0], client_inf[1]
+    print(f'соединяю с пользователем {client_ip, client_port}')
+    print(f'[*] получено новое сообщение: {data}')
+
+    client_sock.send(b'hello')
+    client_sock.close()
